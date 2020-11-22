4e2b6f88d6130c903a94f21e465b608ab1e66135
shenxiang.feng <shenxiang.feng@binance.com>
2020-11-22 17:18:45 +0800

    update

---
+++ Software/Project/cli-test/boo.config.js
@@ -0,0 +1,6 @@
+const { scope } = require('./scope')
+const { commitType } = require('./commit-type');
+module.exports = {
+	scope, 
+	commitType
+}
---
+++ Software/Project/cli-test/scope.js
@@ -0,0 +1,14 @@
+exports.scope = [
+  {
+    name: 'Project 1',
+    description: 'Project 1',
+  },
+  {
+    name: 'Project 2',
+    description: 'Project 2',
+  },
+  {
+    name: 'Project 3',
+    description: 'Project 3',
+  },
+]
